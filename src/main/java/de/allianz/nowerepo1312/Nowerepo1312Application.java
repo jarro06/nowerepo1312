216ace08c93c81c2f71dbf3d936577af0642551c
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-12-13 09:44:54 +0100

    Create maven project

---
+++ src/main/java/de/allianz/nowerepo1312/Nowerepo1312Application.java
@@ -0,0 +1,13 @@
+package de.allianz.nowerepo1312;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nowerepo1312Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nowerepo1312Application.class, args);
+	}
+
+}
